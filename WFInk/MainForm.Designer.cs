5099996dc8be9d1fdb8b60327404c77a09140b90
wxz <sclshu3714@163.com>
2021-03-26 17:08:10 +0800

    INKT 测试

--- WFInk/MainForm.Designer.cs
+++ WFInk/MainForm.Designer.cs
@@ -32,12 +32,11 @@ namespace WFInk
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.axInkPicture1 = new AxMSINKAUTLib.AxInkPicture();
             this.axInkEdit1 = new AxINKEDLib.AxInkEdit();
-            this.canvas = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelView = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.axInkPicture1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axInkEdit1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // axInkPicture1
@@ -51,22 +50,11 @@ namespace WFInk
             // axInkEdit1
             // 
             this.axInkEdit1.Enabled = true;
-            this.axInkEdit1.Location = new System.Drawing.Point(413, 12);
+            this.axInkEdit1.Location = new System.Drawing.Point(12, 280);
             this.axInkEdit1.Name = "axInkEdit1";
             this.axInkEdit1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axInkEdit1.OcxState")));
-            this.axInkEdit1.Size = new System.Drawing.Size(380, 472);
+            this.axInkEdit1.Size = new System.Drawing.Size(380, 211);
             this.axInkEdit1.TabIndex = 1;
-            // 
-            // canvas
-            // 
-            this.canvas.BackColor = System.Drawing.SystemColors.Control;
-            this.canvas.Image = ((System.Drawing.Image)(resources.GetObject("canvas.Image")));
-            this.canvas.Location = new System.Drawing.Point(13, 290);
-            this.canvas.Name = "canvas";
-            this.canvas.Size = new System.Drawing.Size(382, 200);
-            this.canvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.canvas.TabIndex = 2;
-            this.canvas.TabStop = false;
             // 
             // richTextBox1
             // 
@@ -83,21 +71,28 @@ namespace WFInk
             this.flowLayoutPanel1.Size = new System.Drawing.Size(780, 31);
             this.flowLayoutPanel1.TabIndex = 4;
             // 
+            // panelView
+            // 
+            this.panelView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelView.Location = new System.Drawing.Point(401, 12);
+            this.panelView.Name = "panelView";
+            this.panelView.Size = new System.Drawing.Size(392, 479);
+            this.panelView.TabIndex = 5;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(805, 611);
+            this.Controls.Add(this.panelView);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.canvas);
             this.Controls.Add(this.axInkEdit1);
             this.Controls.Add(this.axInkPicture1);
             this.Name = "MainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.axInkPicture1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axInkEdit1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -106,9 +101,9 @@ namespace WFInk
 
         private AxMSINKAUTLib.AxInkPicture axInkPicture1;
         private AxINKEDLib.AxInkEdit axInkEdit1;
-        private System.Windows.Forms.PictureBox canvas;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panelView;
     }
 }
 
--- WFInk/MainForm.cs
+++ WFInk/MainForm.cs
@@ -9,123 +9,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Windows.Forms.Integration;
 
 namespace WFInk
 {
-	internal struct PointInfo
-	{
-		public Point pt; //点的位置
-		public System.Windows.Vector dir; //与上一个点之间的方向
-		public float len; //上一个点到这个点的距离
-		public System.Windows.Vector perpendicular; //与上一个点之间的方向的垂直
-		public System.Drawing.Point ToDrawingPoint()
-		{
-			return new System.Drawing.Point(Convert.ToInt32(pt.X), Convert.ToInt32(pt.Y));
-		}
-	}
 	public partial class MainForm : Form
     {
-        private Point ptStart; //起始点
-        private Point ptEnd; //终点
-		private List<PointInfo> ptinfos = new List<PointInfo>(); //点的信息
 		public MainForm()
         {
             InitializeComponent();
-            this.Load += MainForm_Load;
-			this.canvas.MouseDown += canvas_MouseDown;
-			this.canvas.MouseUp += canvas_MouseUp;
-			this.canvas.MouseMove += Canvas_MouseMove;
+			this.Load += MainForm_Load;
 		}
-        private void canvas_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
-		{
-			ptEnd = e.Location;
-			
-
-			IsPressed = false;
-		}
-
-		private float lw = 5.0F;
-		private bool IsPressed = false;
-
-		private void Canvas_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
-		{
-			Point pt = e.Location;
-			
-		}
-		private void canvas_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
-		{
-			
-			IsPressed = true;
-		}
-
 		private void MainForm_Load(object sender, EventArgs e)
 		{
-
-			InitRecognizer();
-		}
-		private Recognizers myRecognizers; // REM 所有识别器
-		private Microsoft.Ink.Ink ink = new Microsoft.Ink.Ink(); // REM 墨迹
-		private Strokes strokes; // REM 所有的笔画
-		private RecognizerContext recognizectx; // REM 识别器上下文
-												// Private myInkCollector As InkCollector
-												// REM 初始化墨迹识别器
-		public void InitRecognizer()
-		{
-			myRecognizers = new Recognizers();
-			strokes = ink.CreateStrokes();
-			recognizectx = myRecognizers.GetDefaultRecognizer().CreateRecognizerContext();
-			recognizectx.Strokes = strokes;
-		}
-
-		private string[] Regconize() // REM 识别 ptinfos 中的点
-		{
-			List<Point> listpts = new List<Point>();
-            foreach (var i in ptinfos)
-                listpts.Add(i.ToDrawingPoint());
-            // REM 每一笔转换成stroke
-            Stroke stroke = ink.CreateStroke(listpts.ToArray());
-			// REM 添加到识别器的上下文
-			recognizectx.Strokes.Add(stroke);
-			RecognitionStatus recognizestatus = new RecognitionStatus();
-			RecognitionResult recognizeresult = recognizectx.Recognize(out recognizestatus);
-			// REM 识别器的所有选择
-			RecognitionAlternates recognizealternates = recognizeresult.GetAlternatesFromSelection();
-			// REM 列出识别器所识别出的内容
-			List<string> result = new List<string>();
-			for (var i = 0; i <= recognizealternates.Count - 1; i++)
-			{
-				string text = recognizealternates[i].ToString();
-				// Console.WriteLine(text)
-				result.Add(text);
-			}
-			return result.ToArray();
-		}
-
-		private void buttonClear_Click(object sender, EventArgs e)
-		{
-			
-		}
-		private void buttonUndo_Click(object sender, EventArgs e)
-		{
-			
-		}
-
-		public void CreateNewBitmap()
-		{
-			
-		}
-		private byte[] BitmapSourceToByteArray(System.Windows.Media.Imaging.BitmapSource bmpSrc)
-		{
-			var encoder = new System.Windows.Media.Imaging.JpegBitmapEncoder();
-			encoder.QualityLevel = 100;
-			encoder.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(bmpSrc));
-
-			using (var stream = new MemoryStream())
-			{
-				encoder.Save(stream);
-				return stream.ToArray();
-			}
+			InkControl ink = new InkControl();
+			ink.Width = this.panelView.Width;
+			ink.Height = this.panelView.Height;
+			ElementHost _elemHost = new ElementHost();
+			_elemHost.Dock = DockStyle.Fill;
+			_elemHost.Child = ink; // 绑定
+			this.panelView.Controls.Add(_elemHost);
+			_elemHost.Show();
 		}
 	}
 }
