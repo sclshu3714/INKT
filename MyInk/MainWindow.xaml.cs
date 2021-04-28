be1a16f8f29b53c1c206ea32fd6ddb491a63f066
wxz <sclshu3714@163.com>
2021-04-28 23:20:06 +0800

    ============================

--- MyInk/MainWindow.xaml.cs
+++ MyInk/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Ink;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,6 +24,33 @@ namespace MyInk
     /// </summary>
     public partial class MainWindow : Window
     {
+        ///win32 api
+        private const int WS_EX_TRANSPARENT = 0x20;
+        private const int GWL_EXSTYLE = (-20);
+        private const uint WS_EX_LAYERED = 0x80000;
+        private const int GWL_STYLE = (-16);
+        private const int LWA_ALPHA = 0;
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, long dwNewLong);
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int nIndex);
+
+        [DllImport("user32", EntryPoint = "SetLayeredWindowAttributes")]
+
+        private static extern int SetLayeredWindowAttributes(IntPtr hwnd,int crKey,int bAlpha,int dwFlags);
+
+        ///
+        /// 设置窗体具有鼠标穿透效果
+        ///
+        public void SetPenetrate()
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_TRANSPARENT | WS_EX_LAYERED);
+            SetLayeredWindowAttributes(hwnd, 0, 100, LWA_ALPHA);
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -38,7 +67,25 @@ namespace MyInk
             drawingAttributes.IgnorePressure = true;
             drawingAttributes.FitToCurve = true;
             inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            //this.SourceInitialized += delegate
+            //{
+            //    IntPtr hwnd = new WindowInteropHelper(this).Handle; 
+            //    uint extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            //    SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+            //};
+            //SourceInitialized += delegate
+            //{
+            //    IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            //    //long OldLong = GetWindowLong(hwnd,(-20));
+            //    SetWindowLong(hwnd, (-20), 0x20);
+            //};
         }
+
+        //protected override void OnSourceInitialized(EventArgs e) {
+        //    IntPtr hwnd = new WindowInteropHelper(this).Handle;
+        //    //long OldLong = GetWindowLong(hwnd,(-20));
+        //    SetWindowLong(hwnd, (-20), 0x20);
+        //}
         void btn_Click(object sender, RoutedEventArgs e)
         {
             ImageSource imageSource = new BitmapImage(new Uri(@"C:\Users\33169\Pictures\SharedScreenshot.jpg", UriKind.Absolute));
