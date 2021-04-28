9771edc0551851d778e65781f030d3793d33077f
wxz <sclshu3714@163.com>
2021-04-28 22:08:36 +0800

    ===========================

---
+++ CaptureScreenDemo/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaptureScreenDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly RisCaptureLib.ScreenCaputre screenCaputre = new RisCaptureLib.ScreenCaputre();
+        private Size? lastSize;
+        public MainWindow()
+        {
+            InitializeComponent();
+            screenCaputre.ScreenCaputred += OnScreenCaputred;
+            screenCaputre.ScreenCaputreCancelled += OnScreenCaputreCancelled;
+        }
+
+        private void OnScreenCaputreCancelled(object sender, System.EventArgs e)
+        {
+            Show();
+            Focus();
+        }
+
+        private void OnScreenCaputred(object sender, RisCaptureLib.ScreenCaputredEventArgs e)
+        {
+            //set last size
+            lastSize = new Size(e.Bmp.Width, e.Bmp.Height);
+
+
+            Show();
+
+            //test
+            var bmp = e.Bmp;
+            var win = new Window { SizeToContent = SizeToContent.WidthAndHeight, ResizeMode = ResizeMode.NoResize };
+
+            var canvas = new Canvas { Width = bmp.Width, Height = bmp.Height, Background = new ImageBrush(bmp) };
+
+            win.Content = canvas;
+            win.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            Thread.Sleep(300);
+            screenCaputre.StartCaputre(30, lastSize);
+        }
+    }
+}
---
+++ RisCaptureLib/BitmapCursor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace RisCaptureLib
+{
+    internal class BitmapCursor : SafeHandle
+    {
+
+        public override bool IsInvalid
+        {
+            get
+            {
+                return handle == (IntPtr)(-1);
+            }
+        }
+         
+
+        public static Cursor CreateBmpCursor(System.Drawing.Bitmap cursorBitmap)
+        {
+
+            var c = new BitmapCursor(cursorBitmap);
+
+            return CursorInteropHelper.Create(c);
+        }
+
+
+
+        protected BitmapCursor(System.Drawing.Bitmap cursorBitmap)
+            : base((IntPtr)(-1), true)
+        {
+            handle = cursorBitmap.GetHicon();
+        }
+
+
+        protected override bool ReleaseHandle()
+        {
+            bool result = DestroyIcon(handle);
+
+            handle = (IntPtr)(-1);
+
+            return result;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr hIcon);
+
+        public static Cursor CreateCrossCursor()
+        {
+            const int w = 25;
+            const int h = 25;
+
+            var bmp = new System.Drawing.Bitmap(w, h);
+
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bmp);
+            g.SmoothingMode = SmoothingMode.Default;
+            g.InterpolationMode = InterpolationMode.High;
+
+            var pen= new System.Drawing.Pen(System.Drawing.Brushes.Black, 2);
+            g.DrawLine(pen, new Point(12, 0), new Point(12, 8)); // vertical line 
+            g.DrawLine(pen, new Point(12, 17), new Point(12, 25)); // vertical line
+            g.DrawLine(pen, new Point(0, 12), new Point(8, 12)); // horizontal line 
+            g.DrawLine(pen, new Point(16, 12), new Point(24, 12)); // horizontal line
+            g.DrawLine(pen, new Point(12, 12), new Point(12, 13)); // Middle dot 
+
+            g.Flush();
+            g.Dispose();
+            pen.Dispose();
+
+            var c = CreateBmpCursor(bmp);
+
+            bmp.Dispose();
+
+            return c;
+        }
+    }
+}
