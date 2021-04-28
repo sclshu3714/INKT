e25fb1e9210e23fd56c1c3bb75a1c243cd7549e8
wxz <sclshu3714@163.com>
2021-04-28 22:33:56 +0800

    ==================

--- RisCaptureLib/Config.cs
+++ RisCaptureLib/Config.cs
@@ -6,8 +6,9 @@ namespace RisCaptureLib
     internal static class Config
     {
         public static Brush SelectionBorderBrush = new SolidColorBrush(Color.FromArgb(255, 49, 106, 196));
-        public static Thickness SelectionBorderThickness = new Thickness(2.0); 
+        public static Thickness SelectionBorderThickness = new Thickness(2.0);
 
-        public static Brush MaskWindowBackground = new SolidColorBrush(Color.FromArgb(120, 255, 255, 255));
+        //public static Brush MaskWindowBackground = new SolidColorBrush(Color.FromArgb(120, 255, 255, 255));
+        public static Brush MaskWindowBackground = new SolidColorBrush(Color.FromArgb(200, System.Drawing.Color.Black.R, System.Drawing.Color.Black.G, System.Drawing.Color.Black.B));
     }
 }
